37a3fb5acba58e4e6d1dba36f61742f23f69484c
Ching-yu Lin <ching.yulin@cognite.com>
2020-10-22 22:19:34 +0200

    TODOs

--- train.py
+++ train.py
@@ -30,6 +30,9 @@ class Trainer(TorchModelBase):
         return Dataset(contents, labels)
 
     def fit(self, contents, labels):
+        # TODO: validation set, loss, accuracy
+        # TODO: the model doesn't converge
+        # TODO: checkpoints
 
         self.dataset = self.build_dataset(contents, labels)
         self.model = self.build_graph()
@@ -54,6 +57,7 @@ class Trainer(TorchModelBase):
         return
 
     def predict(self):
+        # TODO: test accuracy and save predicted output
         raise NotImplementedError
 
 
