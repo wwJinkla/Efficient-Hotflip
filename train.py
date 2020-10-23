7d71fb4be9fcb82fcbbb6947f13776e088cdad5b
Ching-yu Lin <ching.yulin@cognite.com>
2020-10-23 12:56:57 +0200

    implement training val logic and model saving

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import random
+from collections import Counter
 from typing import List
 
 import torch
@@ -16,7 +17,7 @@ class Trainer(TorchModelBase):
         embed_size,
         hidden_size,
         max_word_length,
-        **model_kwargs
+        **model_kwargs,
     ):
         super().__init__(**model_kwargs)
         self.vocab = vocab
@@ -41,8 +42,15 @@ class Trainer(TorchModelBase):
     def fit(self, contents, labels):
         # TODO: validation set, loss, accuracy
         # TODO: checkpoints
+        (
+            train_contents,
+            train_labels,
+            val_contents,
+            val_labels,
+        ) = self._build_validation_split(contents, labels)
+        self.train_set = self.build_dataset(train_contents, train_labels)
+        self.val_set = self.build_dataset(val_contents, val_labels)
 
-        self.dataset = self.build_dataset(contents, labels)
         self.model = self.build_graph()
         self.optimizer = self.build_optimizer()
 
@@ -53,9 +61,9 @@ class Trainer(TorchModelBase):
             total_losses = 0.0
 
             for batch_step in range(1, 20):
-                sentences, labels = self.dataset[self.batch_size]
+                contents, labels = self.train_set[self.batch_size]
 
-                pred = self.model(sentences)
+                pred = self.model(contents)
                 losses = self.loss(pred, labels)
                 losses.backward()
                 total_losses += losses.item()
@@ -66,14 +74,72 @@ class Trainer(TorchModelBase):
                 self.optimizer.step()
                 self.optimizer.zero_grad()
 
-            accuracy = float((torch.argmax(pred, dim=1) == labels).float().mean())
-            print("iter:", iter_step, "loss:", total_losses, "accuracy", accuracy)
+            # Only on last batch step
+            predicted_labels = torch.argmax(pred, dim=1)
+            accuracy = float((predicted_labels == labels).float().mean())
+
+            val_contents, val_labels = self.val_set[self.batch_size]
+            val_losses, val_accuracy = self.predict(val_contents, val_labels)
+            print(
+                "Iter:",
+                iter_step,
+                "train loss:",
+                losses.item(),
+                "train acc:",
+                accuracy,
+                "val loss:",
+                val_losses,
+                "val acc:",
+                val_accuracy,
+            )
+            if iter_step % 10 == 0:
+                model_path = f"checkpoints/model_iter_{iter_step}"
+                self.to_pickle(model_path)
+                print("Model saved to:", model_path)
 
         return
 
-    def predict(self):
-        # TODO: test accuracy and save predicted output
-        raise NotImplementedError
+    def predict(self, contents, labels):
+        self.model.eval()
+        with torch.no_grad():
+            # TODO: smaller batch if OOM
+            pred = self.model(contents)
+            losses = self.loss(pred, labels).item()
+            predicted_labels = torch.argmax(pred, dim=1)
+            accuracy = float((predicted_labels == labels).float().mean())
+        self.model.train()
+        return losses, accuracy
+
+    @staticmethod
+    def _build_validation_split(contents, labels, validation_fraction=0.1):
+        """Perform a static split for training and validation sets
+        """
+
+        assert len(contents) == len(labels)
+        all_train = list(zip(contents, labels))
+        random.seed(0)
+        random.shuffle(all_train)
+        split_index = int(len(all_train) * validation_fraction)
+
+        train = all_train[split_index:]
+        val = all_train[:split_index]
+
+        train_contents = [c for c, l in train]
+        train_labels = [l for c, l in train]
+
+        val_contents = [c for c, l in val]
+        val_labels = [l for c, l in val]
+
+        print(
+            "train set distribution:",
+            sorted(Counter(train_labels).most_common(), key=lambda x: x[0]),
+        )
+        print(
+            "val set distribution:",
+            sorted(Counter(val_labels).most_common(), key=lambda x: x[0]),
+        )
+
+        return train_contents, train_labels, val_contents, val_labels
 
 
 class Dataset(torch.utils.data.Dataset):
@@ -111,5 +177,6 @@ if __name__ == "__main__":
         batch_size=100,
         eta=0.01,
         max_grad_norm=1,
+        max_iter=50,
     )
     trainer.fit(train_contents, train_labels)
